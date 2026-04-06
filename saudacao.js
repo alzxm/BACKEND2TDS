80dd2b67699e9fb71c248e358b25a0acd2753251
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-04-06 13:50:09 +0000

    v1

---
+++ saudacao.js
@@ -0,0 +1,24 @@
+//Declaração da função com parâmetros
+function saudacao(nomeJogador) {
+    console.log("Bem vindo ao jogo, " + nomeJogador + "!")
+};
+
+saudacao("Khilie");
+
+//Função que recebe um valor, soma 1 e retorna
+function subirDeNivel(nivelAtual) {
+    return nivelAtual + 1
+};
+
+let meuNivel = 10; 
+
+let nivelAtualizado = subirDeNivel(meuNivel);
+console.log("Seu nível atual é: " + nivelAtualizado);
+
+//Função que suma 2 números
+ function soma(numero1, numero2) {
+    return numero1 + numero2
+ }
+
+ console.log(soma(5,3));
+ 
