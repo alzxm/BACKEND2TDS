25fe8f84281f2cbb41758503dea1d8505ff53fee
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-05-11 13:55:55 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,16 @@
+//Viagem
+const destino = "Roma";
+const nomeDoViajante = "Alejandra";
+const cidadeDeOrigem = "Curitiba";
+let duracao = 7;
+let transporte = "avião";
+
+console.log(`A viajante ${nomeDoViajante} deseja viajar de ${cidadeDeOrigem} até ${destino} de ${transporte}, em um percuso de ${duracao} horas.`);
+
+//Custos
+const precoDaPassagem = "14.000";
+let precoDaHospedagem = 20;
+let alimentacao = 60;z
+const total = precoDaPassagem + precoDaHospedagem + alimentacao
+
+    console.log(`O preço da passagem será de R$${precoDaPassagem}, a hospedagem será de €${precoDaHospedagem} e a alimentação será €${alimentacao}. Portanto, o preço total será: ${total} `);
