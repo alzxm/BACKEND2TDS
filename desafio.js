3de8e6d03fdf0f79e99207ddba0da10f9220bbff
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-05-13 13:55:37 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,15 @@
+let preco = 12;
+const nomeDoProduto = "Limpol";
+let numeracaoDoLote = 13579;
+let validadeDoProduto = "01/07/27";
+let quantidadeDoEstoque = 50;
+
+console.log(`Atenção! O produto ${nomeDoProduto} (Lote: ${numeracaoDoLote}, Validade: ${validadeDoProduto}) está disponível por apenas R$${preco}!`);
+
+console.log(`Estoque limitado: apenas ${quantidadeDoEstoque} unidades. Garanta o seu agora!`);,
+
+let precoInicial = 12;
+const desconto = 0.3
+const precoComDesconto = precoInicial - desconto;
+
+console.log(`Além disso, temos 3% de desconto. Então, você irá levar o ${}')
