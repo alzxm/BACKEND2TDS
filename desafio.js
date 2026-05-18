6c64aea47a96227cb6e8e214413fcf552ab8f4f8
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-05-18 13:52:14 +0000

    v2

--- desafio.js
+++ desafio.js
@@ -1,3 +1,4 @@
+// 1. Declarar 5 variáveis para representar um produto.
 let preco = 12;
 const nomeDoProduto = "Limpol";
 let numeracaoDoLote = 13579;
@@ -6,10 +7,22 @@ let quantidadeDoEstoque = 50;
 
 console.log(`Atenção! O produto ${nomeDoProduto} (Lote: ${numeracaoDoLote}, Validade: ${validadeDoProduto}) está disponível por apenas R$${preco}!`);
 
-console.log(`Estoque limitado: apenas ${quantidadeDoEstoque} unidades. Garanta o seu agora!`);,
+console.log(`Estoque limitado: apenas ${quantidadeDoEstoque} unidades. Garanta o seu agora!`);
+
+// 2. Crie dua variáveis e exiba uma subtração.
 
 let precoInicial = 12;
 const desconto = 0.3
 const precoComDesconto = precoInicial - desconto;
 
-console.log(`Além disso, temos 3% de desconto. Então, você irá levar o ${}')
+console.log(`Além disso, temos 3% de desconto. Então, você irá levar o ${nomeDoProduto} por ${precoComDesconto}`);
+
+// 3. Declarar preço e quantidade. Calcular o valor total, o valor com 20% de desconto, quantos produtos inteiros se compram com R$ 500,00 e quanto sobra de troco.
+
+const produto = "Caderno";
+let precoSecundario = 40;
+let quantidade = 60;
+
+
+
+
